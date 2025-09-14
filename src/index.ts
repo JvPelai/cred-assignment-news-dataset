c5889c120210ed6a07baa5bb048a14218911ae06
João Victor Pelai <jv.pelai@gmail.com>
2025-09-14 15:47:44 -0300

    apollo-express integration

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,19 @@ import dotenv from 'dotenv';
 import { readFileSync } from 'node:fs';
 import { join } from 'node:path';
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
 import { resolvers } from './graphql/resolvers';
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { expressMiddleware } from '@as-integrations/express4';
 
 dotenv.config();
 
 async function startServer() {
+  const app = express();
+  const httpServer = http.createServer(app);
+
   // Load GraphQL type definitions
   const typeDefs = readFileSync(join(__dirname, 'graphql', 'schema.graphql'), 'utf8');
 
@@ -15,13 +22,27 @@ async function startServer() {
   const apollo = new ApolloServer({
     typeDefs,
     resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    introspection: true,
   });
+
+  await apollo.start();
+
+  app.use(cors());
+  app.use(express.json());
+
+  // GraphQL endpoint
+  app.use(
+    '/graphql',
+    expressMiddleware(apollo, {
+      context: async () => ({}), // or createContext()
+    })
+  );
+  // Apply Express middleware
   const PORT = process.env.PORT ? Number(process.env.PORT) : 4000;
-  const { url } = await startStandaloneServer(apollo, {
-    listen: { port: PORT },
-    context: async () => ({}),
-  });
-  console.log(`GraphQL endpoint ready at ${url}`);
+  await new Promise<void>((resolve) => httpServer.listen(PORT, resolve));
+
+  console.log(`Server ready at http://localhost:${PORT}/graphql`);
 }
 
 startServer().catch((err) => {
